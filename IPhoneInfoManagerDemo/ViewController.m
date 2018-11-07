18a85a13c0681888f3d6160a7480469fca91bae8
lecco <leccoer@gmail.com>
2018-11-07 15:31:33 +0800

    Initial Commit

---
+++ IPhoneInfoManagerDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  IPhoneInfoManagerDemo
+//
+//  Created by lecco on 2018/11/7.
+//  Copyright © 2018 lecco. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
